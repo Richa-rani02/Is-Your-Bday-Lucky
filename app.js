521cf33f218dbe91e1045a910bf0f582f06ed2c2
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2021-08-26 16:58:31 +0530

    refactor:code updated

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ var birthDate = document.querySelector("#birth-date");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkBtn = document.querySelector("#check-btn");
 var resultTxt = document.querySelector("#result");
+var sadimg = document.querySelector("#sadimg");
+var happyimg = document.querySelector("#happyimg");
+
+hide(sadimg);
+hide(happyimg);
+
+
+
+function show(input, typ) {
+    input.style.display = typ;
+}
+
+function hide(input) {
+    input.style.display = "none";
+}
 
 checkBtn.addEventListener("click", function validate() {
 
     var s = sum(birthDate.value);
     if (s % (luckyNumber.value) === 0) {
-        resultTxt.innerText = "Your birthday is luckyy🥳🥳";
+        resultTxt.innerText = "Your birthday is luckyy";
+        show(happyimg, "block");
+        hide(sadimg);
 
 
     } else {
-        resultTxt.innerText = "Your birthday is not that luckyy😞😞";
+        resultTxt.innerText = "Your birthday is not that luckyy";
+        show(sadimg, "block");
+        hide(happyimg);
 
     }
 })
